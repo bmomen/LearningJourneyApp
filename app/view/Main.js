82ac9a9e3e15afb95d20bc2455c892da2dd785ae
bmomen <babakmomen@mail.com>
2014-08-18 15:31:36 +0100

    Fixed Icon

--- app/view/Main.js
+++ app/view/Main.js
@@ -14,6 +14,10 @@ Ext.define('LearningJourney.view.Main', {
                     id: 'infoButton',
                     iconCls: 'info',
                     iconMask: false,
+                    padding:0,
+                    width:30,
+                    height:20,
+                    iconAlign:'center',
                     align: 'right',
                     hidden: false
                 }
