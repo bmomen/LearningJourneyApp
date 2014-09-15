4206e5418ef65a2c7d469166e8e0fb20c2a4e1e6
bmomen <babakmomen@mail.com>
2014-09-15 23:56:48 +0100

    Added Lol to line 34

--- app/view/Articulation.js
+++ app/view/Articulation.js
@@ -31,13 +31,14 @@ Ext.define('LearningJourney.view.Articulation',
                         styleHtmlCls:'articulationText',
                         html:'<h1><u>What is Articulation?</h1></u><br>\n\
                               <p>Articulation is a pathway for students who would like to progress from\n\
-                               a HND (Higher National Diploma) or HNC (Higher National Certificate) directly into 2nd or 3rd year or a degree programme. This is also referred\n\
+                               a HND (Higher lol National Diploma) or HNC (Higher National Certificate) directly into 2nd or 3rd year or a degree programme. This is also referred\n\
                                to as Direct Entry.  </p><br><br>\n\
                               <h1><u>Why Articulate?</h1></u><br>\n\
-                              <p>A matriculation card to access, and borrow resources from, the University library.<br>\n\
-                               A personal IT account, including email address and access to a range of online resources.<br>\n\
-                               Access to University student support and welfare services.<br>\n\
-                               Access to the sports facilities and social events at the University, as well as the Student Association. </p><br><br>'
+                              <p>There are many benefits that come with Articulation. These include:<br>\n\
+                              <p>-A matriculation card to access, and borrow resources from, the University library.<br>\n\
+                               -A personal IT account, including email address and access to a range of online resources.<br>\n\
+                               -Access to University student support and welfare services.<br>\n\
+                               -Access to the sports facilities and social events at the University, as well as the Student Association. </p><br><br>'
                               
                                
                     },
